365fd3d3ddb1982a0d9ddfcf0a0ff6423c274754
Gilles Gandner <libertalia@franceserv.com>
2018-10-15 10:14:37 +0200

    before remodeling

--- symf/public/stylesheets/js/app.js
+++ symf/public/stylesheets/js/app.js
@@ -1,1 +1,22 @@
 $(document).foundation();
+
+function leftWidth() {
+  var logo = $('#top-menu').find('.logo:first-child');
+  var leftWidth = logo.width() + 'px';
+  $('#toAnchor').css('width', leftWidth);
+  console.log(logo);
+  console.log(leftWidth);
+  
+//  var logo = document.querySelector('#top-menu .logo:first-child');
+//  var leftWidth = logo.clientWidth;
+//  
+//  document.getElementById('toAnchor').style.width = leftWidth + 'px';
+//  
+//  console.log(logo);
+//  console.log(leftWidth);
+  
+}
+
+//window.addEventListener('load', leftWidth, false);
+
+$(document).ready(leftWidth());
