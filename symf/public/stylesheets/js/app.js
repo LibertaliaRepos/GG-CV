d88b2a3a0b915251263e26e7a8c2e8f053ec9feb
Gilles Gandner <libertalia@franceserv.com>
2018-10-15 18:54:23 +0200

    start dark theme
    Signed-off-by: Gilles Gandner <libertalia@franceserv.com>

--- symf/public/stylesheets/js/app.js
+++ symf/public/stylesheets/js/app.js
@@ -1,22 +1,44 @@
 $(document).foundation();
 
-function leftWidth() {
-  var logo = $('#top-menu').find('.logo:first-child');
-  var leftWidth = logo.width() + 'px';
-  $('#toAnchor').css('width', leftWidth);
-  console.log(logo);
-  console.log(leftWidth);
+
+$(document).scroll(function() {
+  var toAnchor = $('#toAnchor');
+  var toAnchorPos = toAnchor.offset();
+  var scrollTop = $(document).scrollTop();
+  var leftLogoHeight = $('#left-logo').outerHeight();
+
   
-//  var logo = document.querySelector('#top-menu .logo:first-child');
-//  var leftWidth = logo.clientWidth;
-//  
-//  document.getElementById('toAnchor').style.width = leftWidth + 'px';
-//  
-//  console.log(logo);
-//  console.log(leftWidth);
+  if (scrollTop >= (leftLogoHeight - 50)) {
+      var top = ((scrollTop - leftLogoHeight) + 200) + 'px';
+      toAnchor.css('top', top);
+  }
+});
+
+function skillImageDim() {
+  var skill_elems = $('#skills > section');
+  
+  skill_elems.each(function(index) {  
+    var h2 = $(this).find('h2')[0];
+    var text = $(this).find('p')[0];
+    var img = $(this).find('img')[0];
+    
+    var h2Height = $(h2).outerHeight(true);
+    var textHeight = $(text).outerHeight(true);
+    
+    var blockHeight = h2Height + textHeight + 10;
+    
+    $(img).css('max-height', blockHeight + 'px');
+  });
+  
+  console.log(skill_elems);
   
 }
 
-//window.addEventListener('load', leftWidth, false);
+$(document).ready(skillImageDim());
+$(window).resize(function() {
+  skillImageDim();
+});
 
-$(document).ready(leftWidth());
+
+
+
