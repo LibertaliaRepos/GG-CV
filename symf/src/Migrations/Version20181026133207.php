406c5e7224e2410402aa1059b74ad8049e6969f2
Gilles Gandner <libertalia@franceserv.com>
2018-10-28 09:04:59 +0100

    Migrations versions to actual database
    Signed-off-by: Gilles Gandner <libertalia@franceserv.com>

---
+++ symf/src/Migrations/Version20181026133207.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20181026133207 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP INDEX skill_anchor_IDX ON skill');
+        $this->addSql('ALTER TABLE project CHANGE explanation explanation LONGTEXT NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE project CHANGE explanation explanation VARCHAR(255) NOT NULL COLLATE utf8mb4_unicode_ci');
+        $this->addSql('CREATE UNIQUE INDEX skill_anchor_IDX ON skill (anchor)');
+    }
+}
