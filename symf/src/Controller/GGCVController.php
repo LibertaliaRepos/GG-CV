ab347e270075304f8cc4bf6a3174726d9bba870f
Gilles Gandner <libertalia@franceserv.com>
2018-10-28 09:00:15 +0100

    Add projects pages action and route
    Signed-off-by: Gilles Gandner <libertalia@franceserv.com>

--- symf/src/Controller/GGCVController.php
+++ symf/src/Controller/GGCVController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Entity\Skill;
+use App\Entity\Project;
 
 
 class GGCVController extends AbstractController {
@@ -16,6 +17,15 @@ class GGCVController extends AbstractController {
         $skills = $this->getDoctrine()->getRepository(Skill::class)->findAll();
         
         return $this->render('GGCV/index.html.twig', array('skills' => $skills));
+    }
+    
+    /**
+     * @Route("/projects", name="GGCV_projects")
+     */
+    public function projects() {
+        $projects = $this->getDoctrine()->getRepository(Project::class)->findAll();
+        
+        return $this->render('GGCV/projects.html.twig', array('projects' => $projects));
     }
     
 }
