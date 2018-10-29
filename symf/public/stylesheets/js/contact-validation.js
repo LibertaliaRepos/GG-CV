9a3d7004495422d7cca755c8e4620d982a302aed
Gilles Gandner <libertalia@franceserv.com>
2018-10-29 14:16:41 +0100

    Create ajax post request and responce viewer
    Signed-off-by: Gilles Gandner <libertalia@franceserv.com>

--- symf/public/stylesheets/js/contact-validation.js
+++ symf/public/stylesheets/js/contact-validation.js
@@ -3,23 +3,19 @@ var contactForm = {
   name    :   $('#nameLab input'),
   phone   :   $('#phoneLab input'),
   society :   $('#societyLab input'),
+  subject :   $('#subjectLab input'),
+  message :   $('#contactMSG'),
   reset   :   $('#contactForm input[type=reset]'),
   submit  :   $('#contactForm input[type=submit]'),
   form    :   $('#contactForm'),
+  sent    :   $('#mailSent'),
   
   init: function() {
     this.email.blur(this.testEmail);
     this.editorCounterInit();
     this.form.submit(this.editorTest);
     $(this.form).find('[contenteditable=true]').keyup(this.editorCounter);
-  },
-  
-  emailListen: function() {
-    this.email.blur(this.testEmail);
-  },
-  
-  submitListen: function() {
-    this.form.submit(this.submitTest);
+    this.reset.click(this.editorReset);
   },
   
   testEmail: function() {
@@ -51,16 +47,55 @@ var contactForm = {
     $(alert).attr('id', 'editorHelper')
     var MSGcontent = $('[contenteditable=true]').text();
     
+    
     if (MSGcontent.length < 10 && $('#editorHelper').length == 0) {
       e.preventDefault();
+      
       $(alert).addClass('help-text alert');
       $(alert).html('Votre message doit contenir plus de dix caractères !!');
       $(alert).addClass('callout alert');
       $('#details').append($(alert));
       $(MSGcontent).attr('aria-describedby', 'editorHelper')
     } else if (MSGcontent.length > 10){
+      e.preventDefault();
+      
       $('#editorHelper').remove();
       $(MSGcontent).removeAttr('aria-describedby');
+      
+      
+      var params = contactForm.paramsSerial($(e.target));
+      
+      
+      
+      $.ajax({
+        type: 'POST',
+        url: '/contact/new',
+        data: JSON.stringify(params),
+        contentType: 'application/json',
+        async: true,
+        success: function(response) {          
+          $(contactForm.sent).html(response.response);
+          $(contactForm.sent).attr('aria-hidden', 'false');
+          $(contactForm.sent).attr('data-status', 'true');
+          $(contactForm.sent).delay(5000).fadeOut('slow', function() {
+            $(this).attr('aria-hidden', 'true');
+            $(this).removeAttr('style');
+          });          
+        },
+        error: function(response) {          
+          var json = response.responseJSON;
+          
+          $(contactForm.sent).html(json.response);
+          $(contactForm.sent).attr('aria-hidden', 'false');
+          $(contactForm.sent).attr('data-status', 'false');
+          $(contactForm.sent).delay(5000).fadeOut('slow', function() {
+            $(this).attr('aria-hidden', 'true');
+            $(this).removeAttr('style');
+          });          
+        }
+      });
+      
+      $(contactForm.reset).trigger('click');
     }
   },
   
@@ -89,13 +124,33 @@ var contactForm = {
   
   editorCounter: function() {
     var editableLength = $('[contenteditable=true]').text().length;
-    console.log(editableLength);
     var counter = $('#myCharCounter span');
     var color = (editableLength < 10 ) ? 'red' : 'green';
     
     $(counter).text(editableLength);
     $(counter).css('color', color);
     
+  },
+  
+  editorReset: function() {
+    $(contactForm.message).val('');
+    $('[contenteditable=true]').empty();
+  },
+  
+  paramsSerial: function(target) {
+    
+    var params = {
+      contact: {
+        email: this.email.val(),
+        subject: this.subject.val(),
+        message: this.message.val(),
+      },
+      author: this.name.val(),
+      phone: this.phone.val(),
+      society: this.society.val()
+    }
+    
+    return params;
   }
 }
 
