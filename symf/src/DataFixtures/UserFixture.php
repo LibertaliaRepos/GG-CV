6839bd50fe611bf89af130638f47571d4c0408e1
Gilles Gandner <libertalia@franceserv.com>
2018-10-29 17:27:39 +0100

    Integrate Authentification with dummy users and paths
    Signed-off-by: Gilles Gandner <libertalia@franceserv.com>

---
+++ symf/src/DataFixtures/UserFixture.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Entity\User;
+
+class UserFixture extends Fixture
+{
+    private $passwordEncoder;
+    
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder) {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+    
+    public function load(ObjectManager $manager) {
+        
+        $user = new User();
+        
+        $roles = $user->getRoles();
+        $roles[] = 'ROLE_ADMIN';
+        
+        $user->setRoles($roles);
+        $user->setUsername('admin');
+        $user->setPassword($this->passwordEncoder->encodePassword($user, '123456789'));
+        
+        $user2 = new User();
+        $user2->setUsername('libertalia');
+        $user2->setRoles($user2->getRoles());
+        $user2->setPassword($this->passwordEncoder->encodePassword($user2, '987654321'));
+
+        $manager->persist($user);
+        $manager->persist($user2);
+        
+        $manager->flush();
+    }
+    
+    
+    
+}
