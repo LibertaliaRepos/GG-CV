e7940b56d0763169605c2566104d92dfa727f5bf
Gilles Gandner <libertalia@franceserv.com>
2018-11-02 08:48:59 +0100

    Update skillType ibncluding Image object
    Signed-off-by: Gilles Gandner <libertalia@franceserv.com>

--- symf/src/Form/SkillType.php
+++ symf/src/Form/SkillType.php
@@ -7,6 +7,10 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\ResetType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use App\Entity\Form\SkillForm;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 
 class SkillType extends AbstractType {
     
@@ -16,9 +20,15 @@ class SkillType extends AbstractType {
         $builder->add('title', TextType::class)
                 ->add('anchor', TextType::class)
                 ->add('explanation', TextareaType::class)
+                ->add('picture', FileType::class, array('required' => false))
+                ->add('oldPicture', HiddenType::class)
                 ->add('annuler', ResetType::class)
                 ->add('ajouter', SubmitType::class);
         
+    }
+    
+    public function configureOptions(OptionsResolver $resolver) {
+        $resolver->setDefaults(array('data_class' => SkillForm::class));
     }
     
 }
