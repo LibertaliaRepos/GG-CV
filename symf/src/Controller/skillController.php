decd5c45f6ad283948314d02ca8fe56be77c407f
Gilles Gandner <libertalia@franceserv.com>
2018-11-04 14:34:28 +0100

    Delete skill
    Signed-off-by: Gilles Gandner <libertalia@franceserv.com>

--- symf/src/Controller/skillController.php
+++ symf/src/Controller/skillController.php
@@ -3,35 +3,43 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use App\Entity\Skill;
+use App\Entity\Skill_Image;
+use Symfony\Component\HttpFoundation\Response;
 
 
 class skillController extends AbstractController {
     
     /**
-     * @Route("/skill/add", name="skill_add")
+     * @Route(
+     *      "/admin/skill/del/{id}",
+     *      name="GGCV_admin_skill_del",
+     *      requirements={"id"="\d+"}
+     * )
      */
-    public function addSkill() {
+    public function delete($id) {
         
         $em = $this->getDoctrine()->getManager();
+        $skillImage = $this->getDoctrine()->getRepository(Skill_Image::class)->find($id);
         
-        $skill1 = new Skill();
-        $skill1->setTitle('HTML5 / CSS3 / Javascript');
-        $skill1->setAnchor('html-css');
-        $skill1->setExplanation("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.");
+        $filename = $skillImage->getImage()->getFilename();
+        $filename = $this->getParameter('skill_dir').'/'. $filename;
         
-        $skill2 = new Skill();
-        $skill2->setTitle('Javascript');
-        $skill2->setAnchor('javascript');
-        $skill2->setExplanation("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l\'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.");
+        $em->remove($skillImage);
         
-        $em->persist($skill1);
-        $em->persist($skill2);
+        try {
+            $em->flush();
+        } catch (\Exception $e) {
+           
+            return $this->json(
+                    array('deleted' => false, 'error' => $e->getMessage()),
+                    Response::HTTP_CONFLICT
+                );
+        }
         
-        $em->flush();
+        unlink($filename);
         
-        return new Response('Skills added');
+        return $this->json(array('deleted' => true), Response::HTTP_OK);
     }
     
 }
