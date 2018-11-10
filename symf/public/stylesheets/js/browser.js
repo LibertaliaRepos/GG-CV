773d584e70775b8082e530ef61d67b38a2765547
Gilles Gandner <libertalia@franceserv.com>
2018-11-10 15:13:31 +0100

    Adapte IE browser crossing

---
+++ symf/public/stylesheets/js/browser.js
@@ -0,0 +1,6 @@
+// Internet Explorer 6-11
+var isIE = /*@cc_on!@*/false || !!document.documentMode;
+
+if (isIE) {
+  document.write('<link rel="stylesheet" href="/stylesheets/IE/css/app.css" />');
+}
