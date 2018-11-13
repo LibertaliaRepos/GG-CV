e567c87d79f27661fd79bd98897939687ef829ed
Gilles Gandner <libertalia@franceserv.com>
2018-11-13 10:45:29 +0100

    clean project

--- symf/public/stylesheets/js/browser.js
+++
@@ -1,6 +0,0 @@
-// Internet Explorer 6-11
-var isIE = /*@cc_on!@*/false || !!document.documentMode;
-
-if (isIE) {
-  document.write('<link rel="stylesheet" href="/stylesheets/IE/css/app.css" />');
-}
