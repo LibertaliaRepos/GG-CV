f1f372e8ad1ffd553d7d5eabb458541cc7d1992f
Gilles Gandner <libertalia@franceserv.com>
2018-11-19 10:30:43 +0100

    clean project
    Signed-off-by: Gilles Gandner <libertalia@franceserv.com>

--- symf/src/Repository/ProjectRepository.php
+++ symf/src/Repository/ProjectRepository.php
@@ -9,7 +9,6 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
 /**
  * @method Project|null find($id, $lockMode = null, $lockVersion = null)
  * @method Project|null findOneBy(array $criteria, array $orderBy = null)
- * @method Project[]    findAll()
  * @method Project[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class ProjectRepository extends ServiceEntityRepository
