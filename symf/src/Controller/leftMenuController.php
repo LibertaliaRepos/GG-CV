d6191bdd7afe2fa02c1b4ac094b7d6ffc0b380a6
Gilles Gandner <libertalia@franceserv.com>
2018-11-19 10:30:29 +0100

    Use FindAllSkills repository wrapper function to get skills in order.
    Signed-off-by: Gilles Gandner <libertalia@franceserv.com>

--- symf/src/Controller/leftMenuController.php
+++ symf/src/Controller/leftMenuController.php
@@ -4,17 +4,17 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use App\Entity\Skill;
 use App\Entity\Project;
+use App\Entity\Skill_Image;
 
 
 class leftMenuController extends AbstractController {
     
     /**
-     * @Route("/leftMenu/index", name="leftMenu_index")
+     * @Route("/leftMenu/skills", name="leftMenu_index")
      */
-    public function indexLeftMenu() {
-        $skills = $this->getDoctrine()->getRepository(Skill::class)->findAll();
+    public function skillsLeftMenu() {
+        $skills = $this->getDoctrine()->getRepository(Skill_Image::class)->findAllSkills();
         
         return $this->render('menu/leftMenu.html.twig', array('skills' => $skills));
     }
